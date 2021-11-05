360d7c5abcc0a7f198ddcbc59afd22883fb18445
Simon <wsj_simon@163.com>
2021-11-05 15:47:25 +0800

    chore(*): migarate yarn to pnpm, upgrade packages, upgrade rollup plugins

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[t|j]sx?$': [
+      'babel-jest',
+      {
+        presets: ['@babel/preset-env', '@babel/preset-typescript'],
+      },
+    ],
+  },
+  testEnvironment: 'jsdom',
 }
